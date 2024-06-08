0a1a6efb4db565b0cc79b60e01b2e1322909df9b
sainathhiwale <sainathhiwale2@gmail.com>
2024-06-08 13:42:36 +0530

    some changes for jenkins poll SCM testing

--- app/src/main/java/com/examen/kotlinmvvm_master/presentation/ui/ProductDetailFragment.kt
+++ app/src/main/java/com/examen/kotlinmvvm_master/presentation/ui/ProductDetailFragment.kt
@@ -6,12 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.examen.kotlinmvvm_master.R
 import com.examen.kotlinmvvm_master.data.model.allproductshop.ShopItem
 import com.examen.kotlinmvvm_master.databinding.FragmentProductDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class ProductDetailFragment : Fragment() {
     private  var TAG = ProductDetailFragment::class.simpleName
     private lateinit var binding: FragmentProductDetailBinding
@@ -34,6 +35,20 @@ class ProductDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentProductDetailBinding.bind(view)
+        updateViews()
+    }
+
+    private fun updateViews() {
+        binding.productDetailPrice.text = "USD ${shopItem.price}"
+        binding.productDetailTitle.text = shopItem.title
+        binding.productDetailDescription.text = shopItem.description
+
+        Glide.with(binding.productDetailImage)
+            .load(shopItem.image)
+            .into(binding.productDetailImage)
+
+        binding.productDetailRating.text = "${shopItem.rating?.rate}"
+        binding.productDetailReviews.text = "${shopItem.rating?.count} Reviews"
     }
 
 
